a9edf69737210c7fbfbdeca5bce5bd7e6a61b379
KYazykov <konstantin.yazykov@yandex.ru>
2023-01-09 19:27:28 +0300

    fix"Удалить лишний объект"

--- src/Server/HttpTaskManager.java
+++ src/Server/HttpTaskManager.java
@@ -4,19 +4,13 @@ import com.google.gson.Gson;
 import manager.FileBackedTasksManager;
 import manager.HistoryManager;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-
-import manager.InMemoryTaskManager;
-import task.Task;
 
 public class HttpTaskManager extends FileBackedTasksManager {
 
     private final KVTaskClient client;
     private final Gson gson = new Gson();
-    private final InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
+
 
     public HttpTaskManager(HistoryManager historyManager, String path) throws IOException, InterruptedException {
         super(historyManager);
@@ -28,6 +22,6 @@ public class HttpTaskManager extends FileBackedTasksManager {
         client.put("tasks",  gson.toJson(tasks.values()));
         client.put("subtasks", gson.toJson(subtasks.values()));
         client.put("epics", gson.toJson(epics.values()));
-        client.put("history", gson.toJson(inMemoryTaskManager.getHistory()));
+        client.put("history", gson.toJson(getHistory()));
     }
 }
